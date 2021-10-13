96a5e818af0a24e5d1fdf98e841836817f6b07b0
Edwin Irungu <irungundigirigi@gmail.com>
2021-10-13 20:36:01 +0300

    minor changes on carousel

--- components/Carousel.js
+++ components/Carousel.js
@@ -2,17 +2,17 @@ import React from 'react'
 import styles from '../styles/Carousel.module.css';
 import AliceCarousel from 'react-alice-carousel';
 import "react-alice-carousel/lib/alice-carousel.css";
-import Solar from '../public/solar.jpg';
-import SolarHeaters from '../public/solar-heater.jpg';
-import DataCentre from '../public/data-centre.jpg';
+import Solar from '../public/solar.png';
+import SolarHeaters from '../public/solar-heater.png';
+import DataCentre from '../public/data-centre.png';
 import Image from 'next/image';
 
 
 
 const Carousel = () => {
     return (
-        <div>
-            <AliceCarousel autoPlay autoPlayInterval="2000">
+        <div className={styles.Carousel}>
+            <AliceCarousel className={styles.Carousel} autoPlay autoPlayInterval="2000">
                 <Image src={Solar} className={styles.sliderImg}/>
                 <Image src={SolarHeaters} className={styles.sliderImg}/>
                 <Image src={DataCentre} className={styles.sliderImg}/>
