e1e9c8198c55b823d31ec7ddee5a2661faf8a0bf
Edwin Irungu <irungundigirigi@gmail.com>
2021-10-19 22:13:12 +0300

    changes on UI

--- components/footer.js
+++ components/footer.js
@@ -4,7 +4,12 @@ import styles from '../styles/footer.module.css'
 const Footer = () => {
     return (
         <div className = {styles.footer}>
-            <h2>Footer</h2>
+            <div className={styles.footerAdress}>Gramic's Power Systems Limited.
+                P.o box 226 - 00300 Nairobi. <br />
+                Phone; 0724647209,0737898643. <br />
+                Email; gramicspowersystems@gmail.com. <br />
+                Website: www.gramicspowersystems.com.<br />
+            </div>
             
         </div>
     )
