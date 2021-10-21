7b0fc75c171039ccc3c12783d9406f6e2b6ec337
Edwin Irungu <irungundigirigi@gmail.com>
2021-10-21 22:18:12 +0300

    footer updated

--- components/footer.js
+++ components/footer.js
@@ -7,6 +7,15 @@ import epra from '../public/epra.png'
 import nca from '../public/nca.png'
 import rerec from '../public/rerec.png'
 import kplc from '../public/kplc.png'
+import abb from '../public/abb.jpg'
+import schneider from '../public/schneider.png'
+import MoE from '../public/MoE.jpeg'
+
+
+
+
+
+
 import Image from 'next/image'
 
 
@@ -16,9 +25,7 @@ import Image from 'next/image'
 const Footer = () => {
     return (
         <div className = {styles.footer}>
-            
             <div className={styles.partners}>
-                
                 <div className={styles.footerLogoContainer}>
                     <Image src={epra} className={styles.footerLogo} /> 
                 </div>
@@ -31,12 +38,24 @@ const Footer = () => {
                 <div className={styles.footerLogoContainer}>
                     <Image src={rerec} className={styles.footerLogo} /> 
                 </div>
+                <div className={styles.footerLogoContainer}>
+                    <Image src={schneider} className={styles.footerLogo} /> 
+                </div>
+                <div className={styles.footerLogoContainer}>
+                    <Image src={abb} className={styles.footerLogo} /> 
+                </div>
+                <div className={styles.footerLogoContainer}>
+                    <Image src={MoE} className={styles.footerLogo} /> 
+                </div>
             </div>
+            
+            
             <div className={styles.footerAdress}>
                 <div className={styles.footerText}><FaPhone className={styles.footerIcons} /><span>0724647209 0737898643</span></div>
                 <div className={styles.footerText}><IoIosMail className={styles.footerIcons}/><span>office@gramics.co.ke</span></div>
                 <div className={styles.footerText}><MdLocationOn className={styles.footerIcons}/><span>Gill Hse, 1st Floor, Suite 16</span></div>
             </div>
+            
         </div>
     )
 }
