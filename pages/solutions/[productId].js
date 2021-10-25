119c2e6e91ba6fb2e01c87e89c00abd449b823f8
Edwin Irungu <irungundigirigi@gmail.com>
2021-10-25 22:33:41 +0300

    changes reverted

--- pages/solutions/[productId].js
+++ pages/solutions/[productId].js
@@ -1,15 +1,15 @@
 import { useRouter } from 'next/router'
-import { Products } from '../data/products'
+import { Products } from '../../data/products'
 import React from 'react'
 import Image from 'next/image'
-import styles from '../styles/Solutions.module.css'
+import styles from '../../styles/Solutions.module.css'
 
 
 const Solutions = () => {
     const router = useRouter()
     const  pid = router.query.productId
     var product = Products.find(p => p.pId == pid)
-    console.log(product)
+    
 
     return (
     <div className={styles.productHolder}>
