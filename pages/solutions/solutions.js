41f006a9e43ec1180b4d4e5c79330c150e73492c
Edwin Irungu <irungundigirigi@gmail.com>
2021-10-25 22:06:00 +0300

    initial commit

---
+++ pages/solutions/solutions.js
@@ -0,0 +1,13 @@
+import Solutions from "../../components/[productId]"  
+export default function Home() {
+    return (
+      <div >
+        
+       <Solutions />
+          
+        
+      </div>
+    
+      
+    )
+  }
