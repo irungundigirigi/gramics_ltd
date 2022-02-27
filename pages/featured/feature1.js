4759bfe0398cc63b2bed6da354241052ac58b03a
Irungu Ndigirigi <indigirigi@irosh.irosh>
2022-02-27 17:41:51 +0300

    Metatags to feature1 added

--- pages/featured/feature1.js
+++ pages/featured/feature1.js
@@ -49,21 +49,19 @@ const Feature1 = () => {
                 DO SOLAR PANELS WORK ON CLOUDY DAYS?
             </div>
             <div className={styles.blogText}>
-                <div className={styles.line}>
-                </div>
-                <div className={read? styles.paragraph : styles.paragraphOn}>
+                
+                <div className={styles.paragraph}>
                     Adoption of solar panels has taken  the world by storm over the last
                     decade due to the increasing demand for clean energy while potentially cutting
-                    your electric bills. Here in Kenya, solar power has provided
-                    a solution to energy needs in areas with no electricity power. But, can solar work on cloudy days or with minimal sun exposure?
+                    your electric bills.
+                    Here in Kenya, solar power has provided
+                    a solution to energy needs in areas with no electricity power. 
+                    <p> But, can solar work on cloudy days or with minimal sun exposure?
                     Photovoltaic solar panels are more effective in direct sunlight but will still generate 10% to 25% of the normal power output even 
-                    on days with minimal sun exposure.
+                    on days with minimal sun exposure.</p>
                 </div>
             </div>
-        <div className={styles.btnHolder}>
-            <button className={styles.btn} onClick={toggleRead}><FaAngleDown className={styles.arrow} /></button>
-
-        </div>
+       
         </div>
     </div>
 
