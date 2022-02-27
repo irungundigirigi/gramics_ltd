b6a162d1eb2be058a93cc5bff20e69278cf31fe2
Irungu Ndigirigi <indigirigi@irosh.irosh>
2022-02-27 17:16:00 +0300

    Metatags to feature1 added

--- pages/featured/feature1.js
+++ pages/featured/feature1.js
@@ -21,23 +21,18 @@ const Feature1 = () => {
 
         <Head>
 
-        <meta property="og:title" content="Get started with your solar journey." />
-        <meta property="og:description" content="We are commited to providing customized Solar solutions to every client." />
-        <meta property="og:image" content="https://gramics.co.ke/meta_image.jpg" />
+        <meta property="og:title" content="Do solar panels work on cloudy days?" />
+        <meta property="og:description" content="Photovoltaic solar panels are more effective in direct sunlight but will still generate 10% to 25% of the normal power output even on days with minimal sun exposure." />
+        <meta property="og:image" content="https://gramics.co.ke/solar-rainy.jpg" />
   
         <meta name="twitter:card" content="summary_large_image"/>
-        <meta name ="og:url" content="https://gramics.co.ke/"/>
-        <meta name="twitter:title" content="Get started with your solar journey."/>
-        <meta name="twitter:description" content="We are commited to providing customized Solar solutions and electrical installations to every client."/>
+        <meta name ="og:url" content="https://gramics.co.ke/featured/feature1"/>
+        <meta name="twitter:title" content="Do solar panels work on cloudy days?"/>
+        <meta name="twitter:description" content="Photovoltaic solar panels are more effective in direct sunlight but will still generate 10% to 25% of the normal power output even on days with minimal sun exposure."/>
         <meta name="twitter:site" content="@gramics"/>
-        <meta name="twitter:image" content="https://gramics.co.ke/meta_image.jpg"/>
+        <meta name="twitter:image" content="https://gramics.co.ke/solar-rainy.jpg"/>
         <meta name="twitter:creator" content="@irungundigirigi"/>
-  
-  
-        <meta name= "description" content="Gramics - Solar Solutions Electrical Installations" />
-        <meta name= "keywords" content="Gramics,Gramics Power, solar, panels,water,pumping system, renewableenergy, energyKenya"/>
-        <title>Gramics -Solar solutions | Electrical supplies and Installations |</title>
-        <link rel="icon" sizes="32x32" href="icon.png" />
+
         </Head>
   
 
